b55c003b7616cfef01cd53a3e2c065aa017f78af
Ponomarev Denis <desa-196@yandex.ru>
2023-05-20 11:27:37 +0300

    Немного поправил ввод числа с проверкой

--- Task1/main.py
+++ Task1/main.py
@@ -5,10 +5,14 @@ readInt = 0
 while(True):
     try:
         readInt = int(input("Введите трехзначное число:"))
-        if 1000 > readInt > 99:
-            break
-        else:
-            print('Число должно быть трехзначным!')
-            
     except:
         print('Введено не число!')
+        continue
+
+    if 1000 > readInt > 99:
+        break
+    else:
+        print('Число должно быть трехзначным!')
+            
+
+print(readInt)
